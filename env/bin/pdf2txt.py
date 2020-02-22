f3afe83b6d9dff6c92e2ad8e10f1242095c68194
Musaib <musaibreshi@outlook.in>
2020-02-23 03:28:19 +0530

    First Deploy Mnrdev v.01

--- env/bin/pdf2txt.py
+++
@@ -1,4 +0,0 @@
-#!/home/mnr/Documents/Python3/env/bin/python
-# EASY-INSTALL-SCRIPT: 'pdfminer.six==20200124','pdf2txt.py'
-__requires__ = 'pdfminer.six==20200124'
-__import__('pkg_resources').run_script('pdfminer.six==20200124', 'pdf2txt.py')
