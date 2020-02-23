9807def88144c8e6a63760ab88e82188139b42ac
Musaib <musaibreshi@outlook.in>
2020-02-23 14:00:54 +0530

    trying to make it  work1

--- env/lib/python3.7/site-packages/seleniumbase-1.35.3-py3.7.egg/seleniumbase/utilities/selenium_grid/start-grid-hub.bat
+++
@@ -1,1 +0,0 @@
-java -jar selenium-server-standalone.jar -role hub -timeout 180 -browserTimeout 75 -port 4444
--- env/lib/python3.7/site-packages/seleniumbase-1.35.3-py3.7.egg/seleniumbase/utilities/selenium_grid/start-grid-hub.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -jar selenium-server-standalone.jar -role hub -timeout 180 -browserTimeout 75 -port 4444
