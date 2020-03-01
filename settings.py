2f99923430de36255b2bf74b92c83f8a513b42fc
Musaib <musaibreshi@outlook.in>
2020-03-02 01:14:07 +0530

    Changed WE TO i

--- settings.py
+++ settings.py
@@ -4,4 +4,4 @@ MAIL_PORT=995
 MAIL_USE_SSL=True
 MAIL_USERNAME = 'mnrdeveloper@outlook.com'
 MAIL_PASSWORD = 'Mnr@mab1995'
-DEFAULT_MAIL_SENDER = 'someone@example.commnrdeveloper@outlook.com'
+DEFAULT_MAIL_SENDER = 'mnrdeveloper@outlook.com'
